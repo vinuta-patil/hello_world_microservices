915e27df0f13455f2988fd67a7443b6f4173b694
Abhinav <abhinav932002@gmail.com>
2024-09-04 16:00:03 -0700

    Hello-World Microservices

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,5 +1,5 @@
-$hello_url = "http://127.0.0.1:50552/hello"
-$world_url = "http://127.0.0.1:50577/world"
+$hello_url = "http://127.0.0.1:57030/hello"
+$world_url = "http://127.0.0.1:56958/world"
 
 $hello_response = Invoke-WebRequest -Uri $hello_url -UseBasicParsing
 $world_response = Invoke-WebRequest -Uri $world_url -UseBasicParsing
